4f88ce19db5cce5f28b480e73b1986d03ab20b3c
Andreas Halse Gravesen <andreas.gravesen@lego.com>
2023-09-18 12:59:11 +0200

    added some changes and fixed navi

--- Assignments/handin01/src/app/app-routing.module.ts
+++ Assignments/handin01/src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';  
-import { AddCreditCardComponent } from './add-credit-card/add-credit-card.component';  
-import { TransactionsComponent } from './transactions/transactions.component'; 
-import { CreditCardDetailsComponent } from './credit-card-details/credit-card-details.component';  
+import { HomeComponent } from './home/home.component';
+import { AddCreditCardComponent } from './add-credit-card/add-credit-card.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { CreditCardDetailsComponent } from './credit-card-details/credit-card-details.component';
 
 
 
-const routes: Routes = [  
-  { path: '', redirectTo: '/home', pathMatch: 'full' },  
-  { path: 'home', component: HomeComponent },  
-  { path: 'add-credit-card', component: AddCreditCardComponent },  
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'add-credit-card', component: AddCreditCardComponent },
   { path: 'transactions', component: TransactionsComponent },
-  { path: 'card-details/:id', component: CreditCardDetailsComponent },  
-];  
+  { path: 'card-details/:id', component: CreditCardDetailsComponent },
+];
 
 
 @NgModule({
--- Assignments/handin01/src/app/home/home.component.ts
+++ Assignments/handin01/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  creditCardData: any;
 
-}
+  constructor(private http: HttpClient) {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.http.get<any[]>('http://localhost:3001/cards').subscribe(data => {
+      this.creditCardData = data.map((card, index) => ({ id: index + 1, ...card }));
+    });
+  }
+}    
