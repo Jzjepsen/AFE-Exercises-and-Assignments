536238a8f158ff9df3d6bb7ac4f974b2b4e123d0
Andreas Halse Gravesen <andreas.gravesen@lego.com>
2023-09-18 14:09:06 +0200

    added awesome remove stuff

--- Assignments/handin01/src/app/credit-card-details/credit-card-details.component.ts
+++ Assignments/handin01/src/app/credit-card-details/credit-card-details.component.ts
@@ -1,13 +1,44 @@
-import { Component, OnInit } from '@angular/core';  
-import { ActivatedRoute } from '@angular/router';  
-import { CREDIT_CARD_DATA } from 'hand-in-1-ver2023/credit-card-server/src/credit-card.data';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { switchMap } from 'rxjs/operators';
 
-  
-@Component({  
-  selector: 'app-card-details',  
-  templateUrl: './credit-card-details.component.html',  
-  styleUrls: ['./credit-card-details.component.css']  
-})  
-export class CreditCardDetailsComponent{
-    creditCardData = CREDIT_CARD_DATA; 
+@Component({
+  selector: 'app-card-details',
+  templateUrl: './credit-card-details.component.html',
+  styleUrls: ['./credit-card-details.component.css']
+})
+export class CreditCardDetailsComponent implements OnInit {
+  card: any;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+
+  ngOnInit() {
+    this.route.params.pipe(
+      switchMap(params => {
+        const index = +params['id'] - 1;
+        return this.http.get<any[]>('http://localhost:3001/cards').pipe(
+          switchMap(cards => {
+            const cardNumber = cards[index].card_number;
+            console.log("card details: ", cardNumber)
+            return this.http.get(`http://localhost:3001/cards/${cardNumber}`);
+          })
+        );
+      })
+    ).subscribe(card => {
+      this.card = card;
+    }, error => {
+      console.error('Error fetching card details:', error);
+    });
+  }
+
+  deleteCard() {
+    if (window.confirm('Are you sure you want to delete this card?')) {
+      this.http.delete(`http://localhost:3001/cards/${this.card.card_number}`).subscribe(() => {
+        alert('Card deleted successfully!');
+      }, error => {
+        console.error('Error deleting card:', error);
+      });
+    }
+  }
 }  
